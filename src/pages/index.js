95800f3f5e9873752fb0fc848abd3f5bcb7d7b91
kartiksathappan <kartiksathappan@gmail.com>
2023-12-13 11:57:12 -0800

    first!

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,118 +1,76 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
+import { useState, useEffect, useCallback } from 'react';
 
 export default function Home() {
+  const initialColors = ['red', 'green', 'blue', 'yellow', 'pink', 'purple'];
+  const initialAnimals = ['panda', 'bear', 'cat', 'dog', 'elephant', 'frog'];
+
+  // State for the current and next combinations
+  const [currentCombination, setCurrentCombination] = useState({ color: 'purple', animal: 'panda' });
+  const [nextCombination, setNextCombination] = useState({});
+  const [usedAnimals, setUsedAnimals] = useState(['panda']); // Track used animals as an array
+
+  // Function to shuffle an array
+  const shuffleArray = useCallback((array) => {
+    const newArray = array.slice();
+    for (let i = newArray.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [newArray[i], newArray[j]] = [newArray[j], newArray[i]]; // Swap elements
+    }
+    return newArray;
+  }, []);
+
+  // Function to pick a new animal
+  const pickNewAnimal = useCallback((animals) => {
+    return animals[Math.floor(Math.random() * animals.length)];
+  }, []);
+
+  // Function to prepare the next combination
+  const prepareNextCombination = useCallback(() => {
+    let availableAnimals = initialAnimals.filter(animal => !usedAnimals.includes(animal));
+
+    if (availableAnimals.length === 0) {
+      availableAnimals = shuffleArray(initialAnimals);
+      setUsedAnimals(['panda']); // Reset used animals but keep 'panda' since it's the initial animal
+    }
+
+    const nextAnimal = pickNewAnimal(availableAnimals);
+    const nextColor = pickNewAnimal(initialColors);
+    setNextCombination({ color: nextColor, animal: nextAnimal });
+    // Update the used animals with the new animal
+    setUsedAnimals(prevUsedAnimals => [...prevUsedAnimals, nextAnimal]);
+  }, [usedAnimals, initialAnimals, initialColors, pickNewAnimal, shuffleArray]);
+
+  // Initialize the next combination on component mount
+  useEffect(() => {
+    prepareNextCombination();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []); // Empty dependency array ensures this effect only runs once after initial render
+
+  // Function to handle page turn
+  const goToNextPage = () => {
+    setCurrentCombination(nextCombination);
+    prepareNextCombination();
+  };
+
+  // Render the component
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div onClick={goToNextPage} className="flex flex-col h-screen justify-between items-center p-4 bg-white text-black">
+      <h1 className="text-xl font-bold pt-8">
+        {currentCombination.color} {currentCombination.animal}, What do you see?
+      </h1>
+      
+      <div className="flex flex-col items-center justify-center flex-grow">
+        <div className="w-full max-w-sm h-64 bg-gray-200 flex items-center justify-center">
+          <div className="text-5xl text-gray-500">{currentCombination.animal}</div>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+      
+      {/* Check that nextCombination is not an empty object before rendering */}
+      {nextCombination.color && nextCombination.animal && (
+        <p className="text-xl font-light pb-8">
+          I see a {nextCombination.color} {nextCombination.animal} looking at me.
+        </p>
+      )}
+    </div>
+  );
 }
