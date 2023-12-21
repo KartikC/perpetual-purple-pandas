2045d69f359781983feabbac0a2654a65a747a70
kartiksathappan <kartiksathappan@gmail.com>
2023-12-21 18:27:42 -0500

    new icons

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,13 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        {/* Favicon */}
+        <link rel="icon" href="/favicon.ico" />
+
+        {/* OG Image (replace with your actual image path) */}
+        <meta property="og:image" content="/og-image.png" />
+      </Head>
       <body>
         <Main />
         <NextScript />
