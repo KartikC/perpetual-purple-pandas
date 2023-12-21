cbafd3bfffd138aa94068b368b8d1c1e580300ef
kartiksathappan <kartiksathappan@gmail.com>
2023-12-21 18:37:15 -0500

    meta tags

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -8,7 +8,20 @@ export default function Document() {
         <link rel="icon" href="/favicon.ico" />
 
         {/* OG Image (replace with your actual image path) */}
-        <meta property="og:image" content="/og-image.png" />
+  <meta property="og:url" content="https://www.purplepandabook.com/" />
+  <meta property="og:type" content="website" />
+  <meta property="og:title" content="Purple Panda Book" />
+  <meta property="og:description" content="An infinite color & animal book" />
+  <meta property="og:image" content="https://purplepandabook.com/og-image.png" />
+
+  <meta name="twitter:card" content="summary_large_image" />
+  <meta property="twitter:domain" content="purplepandabook.com" />
+  <meta property="twitter:url" content="https://www.purplepandabook.com/" />
+  <meta name="twitter:title" content="Purple Panda Book" />
+  <meta name="twitter:description" content="An infinite color & animal book" />
+  <meta name="twitter:image" content="https://purplepandabook.com/og-image.png" />
+
+        
       </Head>
       <body>
         <Main />
